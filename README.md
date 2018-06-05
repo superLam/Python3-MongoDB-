9ffea3356087a946e17aae89d8eccf6df8dd8873
superLam <513968746@qq.com>
2018-06-05 13:40:57 +0800

    使用mongoDB获取文本数据

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Python3-MongoDB-
+
+利用python3获取mongoDB数据
--- getrank.py
+++ getrank.py
@@ -4,41 +4,29 @@ import sys
 from pymongo import MongoClient
 
 def get_rank(user_id):
+    #读取数据
     client = MongoClient()
     db = client.shiyanlou
     contests = db.contests
 
-    #计算用户 user_id 排名、总分、花费时间
+    #计算总分和时间
     
-    #计算排名
+    times = 0
+    scores = 0
+    for score in db.contests.find({'user_id':user_id}):
+        scores += score['score']
+    for time in db.contests.find({'user_id':user_id}):
+        times += time['submit_time']
+    
 
-
-
-    #计算总分
-    def get_score():
-        total = 0
-        for scores in db.contests.find({'user_id':user_id}):
-            total += scores['score']
-        print total
-
-    #计算时间
-    def get_times():
-        times = 0
-        for time in db.contests.find({'user_id':user_id}):
-            times += time['submit_time']
-        print(times)
-
-    #依次返回排名，分数，时间
-    return rank, score, submit_time
+    #依次返回分数，时间
+    return  scores, times
 
 if __name__ == '__main__':
 
-    '''
-    1. 判断参数格式是否符合要求
-    2. 获取 user_id 参数
-    '''
+    #获取用户id,python3 getrank.py 2,获取的就是id为2
     user_id = sys.argv[1]
 
-    #根据用户ID获取用户排名、分数、时间
-    userdata = get_rank(user_id)
+    #根据用户ID获取分数、时间,需要输入int类型
+    userdata = get_rank(int(user_id))  
     print(userdata)
